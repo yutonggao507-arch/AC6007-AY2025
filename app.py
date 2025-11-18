4b99a5d67a3314af69d290bde49b7244b8d836e5
yutonggao507-arch <yutonggao507@gmail.com>
2025-11-18 02:56:42 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-1
 from flask import Flask, render_template,request
 
 app=Flask(__name__)
@@ -6,6 +5,11 @@ app=Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+@app.route("/main", methods=["GET","POST"])
+def main():
+    r=request.form.get("q")
+    return(render_template("main.html"))
 
 if __name__=="__main__":
     app.run()
